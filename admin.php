54eb100c04f35168b26582b9e43a3a6d18390862
Kawê <kawe.apr@gmail.com>
2023-02-27 02:34:49 -0300

    #administrativo finalizado

--- admin.php
+++ admin.php
@@ -6,65 +6,88 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Página Administrativa</title>
+    <title>Sigma</title>
+    <link rel="icon"
+        href="https://centralsigma.com.br/wp-content/uploads/2021/03/cropped-Logo-SIGMA-b0ff08a9-1-1-32x32.png"
+        sizes="32x32">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-
+    <link rel="stylesheet" href="styles/admin.css" type="text/css">
 </head>
-<style>
-html,
-body {
-    height: 100%;
-    margin: 0;
-}
 
-#sidebar {
-    height: 100vh;
-    width: 20vw;
-}
-
-@media (max-width: 992px) {
-    #sidebar {
-        position: static;
-        height: auto;
-        /* width: 20vw; */
-    }
-}
-</style>
 
 <body>
+
     <div class="container-fluid">
+        <div class="">
+
+        </div>
         <div class="row">
-            <div class="col-md-3 bg-dark text-light " id="sidebar">
-                <h2>Empresa X</h2>
-                <button class="btn btn-primary btn-block" id="btnHome">Home</button>
-            </div>
-            <div class="col-md-9 mt-5">
-                <div class="d-flex justify-content-between">
-                    <div>
-                        <h2>Lista de Usuários</h2>
+            <div class="col-md-2 bg-dark text-light d-flex flex-column justify-content-between" id="sidebar">
+                <div>
+
+                    <div class="d-block mb-3">
+                        <div class="mt-3 mb-1" style="scale:2">
+                            <i class="bi bi-person-circle"></i>
+                        </div>
+                        <span id="session_name"></span>
                     </div>
-                    <div>
-                        <button class="btn btn-primary" id="btnCriarModal">Adicionar Novo Usuário<i
-                                class="ps-2 bi bi-person-add"></i></button>
+                    <a href="#" style="text-decoration:none !important; color:white;" id="btnHome">
+                        <div class="d-block w-100 pt-2 pb-2 mt-2" style="background-color:#3e4042">
+                            Dashboard
+                        </div>
+                    </a>
+                    <a href="#" style="text-decoration:none !important; color:white;">
+                        <div class="d-block w-100 pt-2 pb-2 mt-2 logout" style="background-color:#3e4042">
+                            Sair
+                        </div>
+                    </a>
+                </div>
+                <!-- <div>
+                    <a href=" #" class="logout">Sair</a>
+                </div> -->
+                <!-- <button class="btn btn-primary btn-block" id="btnHome">Home</button> -->
+            </div>
+
+            <div class="col-md-10 ps-0 pe-0">
+                <div class="d-flex w-100 pt-1 ps-3 pe-3 header justify-content-between">
+                    <h2 class="text-light ps-3">
+                        <img style="height: 40px;"
+                            src="https://centralsigma.com.br/wp-content/uploads/2021/08/5179pia_de_logo_sigma_letra_preta_2_.svg"
+                            alt="SIGMA">
+                        SIGMA
+                    </h2>
+                    <div class="d-flex justify-content-center pe-3" style="align-items: center;weight: 40px;scale: 2;">
+                        <a href=" #" class="logout"><i class="bi bi-box-arrow-right"></i></a>
                     </div>
                 </div>
+                <div class="tableBox">
+                    <div class="d-flex justify-content-between mt-3">
+                        <div>
+                            <h2>Dashboard</h2>
+                        </div>
+                        <div class="d-flex center mb-3">
+                            <button class="btn btn-primary" id="btnCriarModal">Adicionar Novo Usuário<i
+                                    class="ps-2 bi bi-person-add"></i></button>
+                        </div>
+                    </div>
 
-                <table class="table table-striped table-bordered" id="tableUsuarios">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nome</th>
-                            <th>Email</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <!-- aqui serão inseridos os dados do DataTable -->
-                    </tbody>
-                </table>
+                    <table class="table table-striped table-bordered" id="tableUsuarios">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Email</th>
+                                <th>Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- aqui serão inseridos os dados do DataTable -->
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
@@ -106,12 +129,7 @@ body {
             </div>
         </div>
     </div>
-    <div class="position-fixed top-0 end-0" style="width: 400px; z-index: 9999; opacity: 0.8;">
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            A modificação ocorreu corretamente!
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    </div>
+
     <!-- Fim do Modal -->
 
     <!-- Modal para Editar os dados do Usuario -->
@@ -178,23 +196,41 @@ body {
             </div>
         </div>
     </div>
+    <div class="position-fixed top-0 end-0" style="width: 400px; z-index: 9999; opacity: 0.8;">
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            A modificação ocorreu corretamente!
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
     <!-- Fim do Modal confirmar -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
+
     <script>
     $(document).ready(function() {
+
+        const token = sessionStorage.getItem('session');
+        if (!token) {
+            window.location = 'index.php'
+        } else {
+            var nome = JSON.parse(atob(token.split('.')[1])).name;
+            $("#session_name").append("Bem vindo(a), ", nome, "!");
+        }
+        const intervalId = setInterval(verificaSessao, 1000 * 10); // chama a função a cada minuto
         $('#modalEditarUsuario').display = 'none';
         $('.alert').addClass('d-none');
         var table = $('#tableUsuarios').DataTable({
+
             ajax: {
                 method: 'POST',
                 url: 'http://localhost/admin_api/',
                 data: {},
                 dataSrc: ''
             },
+            info: false,
             columnDefs: [{
                     width: '5%',
                     targets: [0]
@@ -202,7 +238,7 @@ body {
                 {
                     width: '30%',
                     targets: [1]
-                },
+                }
             ],
             columns: [{
                     data: function(data, type, row, meta) {
@@ -227,6 +263,8 @@ body {
                 }
             ]
         });
+        $('#tableUsuarios_length').css('opacity', 0);
+
         $('#btnCriarModal').click(function() {
             $('#nome_novo').val('');
             $('#email_novo').val('');
@@ -288,6 +326,10 @@ body {
         $('#btnHome').click(function() {
             table.ajax.reload();
         });
+        $('.logout').click(function() {
+            sessionStorage.removeItem('session');
+            window.location = 'index.php';
+        });
     });
 
     function criar() {
@@ -347,6 +389,25 @@ body {
 
     function reload() {
         $('#tableUsuarios').DataTable().ajax.reload()
+    }
+
+
+    function verificaSessao() {
+        const token = sessionStorage.getItem('session');
+        if (token) {
+            const exp = JSON.parse(atob(token.split('.')[1])).exp;
+            const expDate = new Date(exp * 1000);
+            const currentDate = new Date();
+            if (currentDate > expDate) {
+                console.log('Sessão expirou!');
+                sessionStorage.removeItem('session');
+                window.location = 'index.php'
+                // clearInterval(intervalId);
+
+            }
+        } else {
+            window.location = 'index.php'
+        }
     }
     </script>
 </body>
